0b4f7ec741d33e366c5cceadf214a38b1a512325
qualitygurus <70306234+qualitygurus@users.noreply.github.com>
2021-07-09 17:28:05 -0600

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
-file_bytes = st.file_uploader('Upload a file', type='csv')
-
-If file_bytes is not None:
-  data = pd.read_csv(file_bytes)
-  st.write(data)
+ 
+uploaded_file = st.file_uploader("Upload Files",type=['csv'])
+if uploaded_file is not None:
+    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+     st.write(file_details)
