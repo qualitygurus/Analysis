9e33c4869b56d16736a23b75699903f84a916f7f
qualitygurus <70306234+qualitygurus@users.noreply.github.com>
2021-07-09 17:24:55 -0600

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,5 +4,6 @@ import pandas as pd
 
 file_bytes = st.file_uploader('Upload a file', type='csv')
 
-data = pd.read_csv(file_bytes)
-st.write(data)
+If file_bytes is not None:
+  data = pd.read_csv(file_bytes)
+  st.write(data)
